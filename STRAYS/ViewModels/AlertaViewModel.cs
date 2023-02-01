19df2bba707367d630e7e98d9422cdc3eee87a66
Wilixs <115099196+Wilixs@users.noreply.github.com>
2023-01-31 20:45:19 -0500

    Implementadas las clases, AlertaPage, AlertaRegistroPage,AlertaViewModel

---
+++ STRAYS/ViewModels/AlertaViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.Logging;
+using STRAYS.Models;
+using STRAYS.Views;
+
+namespace STRAYS.ViewModels
+{
+    public partial class AlertaViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        public List<AlertaModel> mascota = App.Repositorio.GetAllAlertas();
+
+        [RelayCommand]
+        public void GoToAlertaRegistroPage()
+        {
+            Shell.Current.GoToAsync(nameof(AlertaRegistroPage));
+        }
+
+        [RelayCommand]
+        private void AbrirFlyout()
+        {
+            Shell.Current.FlyoutIsPresented = true;
+        }
+    }
+
+}
+
